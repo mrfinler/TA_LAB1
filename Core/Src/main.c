eda4725bb1ff652714f148a66aa76a994ad6ebbd
mrfinler <49670829+mrfinler@users.noreply.github.com>
2021-02-11 17:38:07 +0700

    last

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "stdio.h"
 
 
 /* USER CODE END Includes */
@@ -54,10 +55,16 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+int _write(int file,char *ptr, int len){
+	int i = 0;
+	for(i = 0;i<len;i++){
+		ITM_SendChar((*ptr++));
+	}
+}
 /* USER CODE END 0 */
 
 /**
+<<<<<<< HEAD
  * @brief  The application entry point.
  * @retval int
  */
@@ -99,6 +106,7 @@ int main(void) {
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
+		printf("hhhh\n");
 		//sw press is LOW
 		if (HAL_GetTick() - samp >= 50) {
 			samp = HAL_GetTick();
@@ -106,7 +114,6 @@ int main(void) {
 			if (Sw_state == 0) {
 				while (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10) == 0)
 					;
-				sprintf("gg\n");
 				count = count + 1;
 				toggle = count % 2;
 			}
@@ -132,6 +139,82 @@ int main(void) {
 		}
 	}
 	/* USER CODE END 3 */
+=======
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  uint8_t Sw_state = 0;
+  uint8_t count = 0;
+  uint8_t toggle = 0;
+  uint32_t timestamp = 0;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  //sw press is LOW
+	  	  Sw_state = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10);
+	  	  if(Sw_state == 0){
+	  		  count = count + 1;
+	  		  toggle = count%2;
+	  	  }
+	  	  if(toggle == 0){
+	  		  if(HAL_GetTick()- timestamp >= 500){
+	  			  timestamp = HAL_GetTick();
+	  			  if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9) == GPIO_PIN_SET){
+	  				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9,GPIO_PIN_RESET);
+	  			  }
+	  			  else
+	  			  {
+	  				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9,GPIO_PIN_SET);
+	  			  }
+	  		  }
+	  	  }
+	  	  else if(toggle == 1){
+	  		  if(HAL_GetTick()- timestamp >= 250){
+	  			  timestamp = HAL_GetTick();
+	  			  if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9) == GPIO_PIN_SET){
+	  				  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9,GPIO_PIN_RESET);
+	  			  }
+	  			  else
+	  			  {
+	  				  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9,GPIO_PIN_SET);
+	  			  }
+	  		  }
+	  	  }
+  }
+  /* USER CODE END 3 */
+>>>>>>> parent of cc23c0b... Led blink with debounce sw
 }
 
 /**
